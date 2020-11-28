a939310ce6b4412c7035858d0b7bf821b4f05973
Diptopol Dam <diptodam@gmail.com>
2020-11-28 00:15:56 -0500

    Added log4j 2 configuration

---
+++ src/main/java/com/refactoringMatcher/init/App.java
@@ -0,0 +1,17 @@
+package com.refactoringMatcher.init;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Diptopol
+ * @since 11/28/2020 12:13 AM
+ */
+public class App {
+
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Init");
+    }
+}
